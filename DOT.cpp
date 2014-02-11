b3aa96e9b5d82bdaaf6fa9b8f9674e51c1bfdb05
arup059 <roy.arup@gmail.com>
2014-02-11 01:40:41 -0500

    Update DOT.cpp

--- DOT.cpp
+++ DOT.cpp
@@ -91,12 +91,8 @@ void prepare(string globalConfFile)
 
 
 	PartitioningAlgorithm * partitioning;
-	if(globalConf->partitioningAlgorithm.compare("SA") == 0)
-		partitioning = new SA(globalConf->partitioningAlgoConfFile, inputTopology, physicalEnvironment, mapping);
-	else if(globalConf->partitioningAlgorithm.compare("SA_Weighted") == 0)
-		partitioning = new SA_Weighted(globalConf->partitioningAlgoConfFile, inputTopology, physicalEnvironment, mapping, hosts);
-	else if(globalConf->partitioningAlgorithm.compare("Guaranteed_Embedding") == 0)
-			partitioning = new GuranteedEmbedding(globalConf->partitioningAlgoConfFile, inputTopology, physicalEnvironment, mapping, hosts);
+	partitioning = new GuranteedEmbedding(globalConf->partitioningAlgoConfFile, 
+		inputTopology, physicalEnvironment, mapping, hosts);
 
 	partitioning->run();
 	// << "IP: " << physicalEnvironment->getIPAddress("cn212") << endl;
@@ -126,45 +122,11 @@ void prepare(string globalConfFile)
 
 	CommandExecutor commandExecutor(credentials, globalConf->masterIPAddress, globalConf->masterName);
 
-	//cout << commandExecutor.executeRemote("nsm2.cs.uwaterloo.ca", "ls") << endl;
-	//commandExecutor.executeLocal("~/temp.sh 100md08");
-	//commandExecutor.copyContent("localhost", "nsm2.cs.uwaterloo.ca", "~/Music/mp3.temp", "~/");
-	//commandExecutor.copyContent("localhost", "nsm2.cs.uwaterloo.ca", "~/Music", "~/", true);
-	//commandExecutor.copyContent( "nsm2.cs.uwaterloo.ca", "localhost", "~/migration", "~/", true);
-	//commandExecutor.copyContent( "nsm2.cs.uwaterloo.ca", "localhost", "~/Portal/about.html", "~/");
-	//cout << commandExecutor.executeScriptLocal("~/", "temp.sh", "100");
-	//cout << commandExecutor.executeScriptRemote("nsm2.cs.uwaterloo.ca", "~/", "temp.sh", "1000k");
-	//commandExecutor.executeLocal("~/temp.sh 100md");
 	InstantitiateSwitch*  instantitiatedSwitch = new OVS_1_9(&commandExecutor);
 	InstantitiateLink* instantitiatedLink = new VLink(&commandExecutor);
 	InstantitiateHost* instantitiatedHost = new LibvirtAttachment(globalConf, hosts, &commandExecutor, inputTopology, mapping);
 
-	//cout << hostAttachment->tapInterfacePrefix() << endl;
-	//hostAttachment->startHost();
-	//cout << Util::intToHexString(27,4)<<endl;
-	/*Switch switch1(1, "br1", mapping->getMachine(0));
-	Switch switch2(2, "g1", mapping->getMachine(0), GATEWAY_SWITCH);
-	Switch switch3(1, "br2", mapping->getMachine(1));
-	Switch switch4(2, "g2", mapping->getMachine(1), GATEWAY_SWITCH);
-	cout << switch1.getDPID() << endl;
-	cout << switch1.getType() << endl;
-
-
-	Interface int1("veth0", &switch1, VETH);
-	Interface int2("veth1", &switch2, VETH);
-
-	Interface int3("gre01", &switch2, GRE);
-	Interface int4("gre10", &switch4, GRE);
-
-	Tunnel tunnel(&int3, &int4);
-
-	CutEdge cEdge(100, &int1, &int2, &tunnel);
-	cout << cEdge.getCutEdgeId() << " "<<  cEdge.getAttachedTunnel()->remoteIP(&cEdge) << endl;
-
-	cout << tunnel.getType() << " " << tunnel.remoteIP(&cEdge) << endl;
-	//cout << cEdge.getCutEdgeId() << " "<<  cEdge.getAttachedTunnel()->getRemoteIP() << endl;
-
-	*/
+	
 
 	DOT_Topology* dotTopology = DOT_Topology::getDOT_Topology(inputTopology, physicalEnvironment,
 			mapping, sw2controller, hosts, instantitiatedHost);
@@ -173,25 +135,6 @@ void prepare(string globalConfFile)
 	DeployDOT *deploy = new DeployDOT(dotTopology, instantitiatedSwitch, instantitiatedLink, instantitiatedHost,  &commandExecutor);
 
 
-	/*cout << Util::getFileName("~/hello/nothing") << endl;
-	cout << Util::getFileName("~/hello/nothing.cpp") << endl;
-	cout << Util::getFileName("/hello/nothing") << endl;
-	cout << Util::getFileName("/hello/nothing.cpp") << endl;
-	cout << Util::getFileName("~/hello/something/nothing") << endl;
-	cout << Util::getFileName("~/hello/something/nothing.cpp") << endl;
-
-
-	cout << Util::getPathName("~/hello/nothing") << endl;
-	cout << Util::getPathName("~/hello/nothing.cpp") << endl;
-	cout << Util::getPathName("/hello/nothing") << endl;
-	cout << Util::getPathName("/hello/nothing.cpp") << endl;
-	cout << Util::getPathName("~/hello/something/nothing") << endl;
-	cout << Util::getPathName("~/hello/something/nothing.cpp") << endl;
-	cout << Util::getPathName("nothing.cpp") << endl;
-	cout << Util::getPathName("nothing") << endl;
-	cout << Util::getPathName("~/") << endl;
-	string str = Util::getPathName("~/").compare("")==0?"True":"False";
-	cout << str << endl;*/
 
 	cout << "end" <<endl;
 
